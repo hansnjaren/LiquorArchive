e85f25723411a1ccecd09185dc5b1492fce50a63
rowletter <hanskimqwerty@gmail.com>
2025-07-15 12:13:48 +0900

    Logout button added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import { TabList } from "./TabList";
 import { Tab } from "./Tab";
 import Link from "next/link";
 import { HEADER_HEIGHT, TAB_LIST_COLOR, TITLE, TITLE_COLOR } from "../constants";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Header({ children }: { children: React.ReactNode }) {
   const height = HEADER_HEIGHT;
@@ -40,11 +40,24 @@ export default function Header({ children }: { children: React.ReactNode }) {
             }
           </div>
         </div>
-        <TabList>
-          <Tab text="Purchase List" dir="purchase-list" />
-          <Tab text="Collections" dir="collections" />
-          <Tab text="Log Calendar" dir="log-calendar" />
-        </TabList>
+        <div>
+          <TabList>
+            <Tab text="Purchase List" dir="purchase-list" />
+            <Tab text="Collections" dir="collections" />
+            <Tab text="Log Calendar" dir="log-calendar" />
+          </TabList>
+          {
+            session ? 
+              <div 
+                className="absolute top-3/4 right-0 transform -translate-y-1/2 p-8 cursor-pointer"
+                onClick={() =>
+                  signOut({ callbackUrl: "/" })
+                }>
+                Logout
+              </div> : 
+              <></>
+          }
+        </div>
       </div>
       <div style={{ height: height }}></div>
 
